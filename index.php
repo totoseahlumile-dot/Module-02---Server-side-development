8b199a54072802d286eaaac1230326ec82aa32e9
ahlumiletotose <totoseahlumile@gmail.com>
2026-07-27 16:23:44 +0200

    Added index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+# TODO 1
+<?php
+$name = "Ahlumile";
+
+echo "My name is $name. My favourite programming language is MySQL because it is easy to learn and use for database management.";
+
+# TODO 2
+$num1 = 99;
+$num2 = 61;
+$sum = $num1 + $num2;
+
+echo "When you add $num1 to $num2 the answer is: $sum";
+
+# TODO 3
+echo "Today is " . date("l, F j, Y") . ".";
+
+?>
+
+# TODO 4
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Page</title>
+</head>
+<body>
+    <h1><?php echo "Welcome to PHP Programming!"; ?></h1>
+</body>
+</html>
+
+# TODO 5
+
+<?php
+$luckyNumber = rand(1, 100);
+echo "Your lucky number today is: $luckyNumber";
+?>
